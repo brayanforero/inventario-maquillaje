0434232877eca72d779be7fdf58527a5d9edc1e7
Brayan Forero <=brayanroswll2802@gmail.com>
2020-07-26 10:03:34 -0400

    Arregla sidebar de tipo user

--- layouts/user_menu.php
+++ layouts/user_menu.php
@@ -1,29 +1,29 @@
 <ul>
-  <li>
+  <li class="p-3 text-center">
     <a href="home.php">
-      <i class="glyphicon glyphicon-home"></i>
+      <i class="fas fa-home text-light h5 mb-0 mr-1"></i>
       <span>Panel de control</span>
     </a>
   </li>
-  <li>
+  <li class="p-3 text-center">
     <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-th-list"></i>
-       <span>Ventas</span>
-      </a>
-      <ul class="nav submenu">
-         <li><a href="sales.php">Administrar ventas</a> </li>
-         <li><a href="add_sale.php">Agregar venta</a> </li>
-     </ul>
+      <i class="fas fas fa-coins text-light h5 mb-0 mr-1"></i>
+      <span>Ventas</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales.php">Administrar ventas</a> </li>
+      <li><a href="add_sale.php">Agregar venta</a> </li>
+    </ul>
   </li>
-  <li>
+  <li class="p-3 text-center">
     <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-signal"></i>
-       <span>Reporte de ventas</span>
-      </a>
-      <ul class="nav submenu">
-        <li><a href="sales_report.php">Ventas por fecha </a></li>
-        <li><a href="monthly_sales.php">Ventas mensuales</a></li>
-        <li><a href="daily_sales.php">Ventas diarias</a> </li>
-      </ul>
+      <i class="fas fa-chart-line text-light h5 mb-0 mr-1"></i>
+      <span>Reporte de ventas</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales_report.php">Ventas por fecha </a></li>
+      <li><a href="monthly_sales.php">Ventas mensuales</a></li>
+      <li><a href="daily_sales.php">Ventas diarias</a> </li>
+    </ul>
   </li>
 </ul>

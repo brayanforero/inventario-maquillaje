0ce36682eac43c8a3a96975bc460d28975035242
Brayan Forero <=brayanroswll2802@gmail.com>
2020-07-26 10:09:17 -0400

    Estilisa el sidebar de tipo special Please enter

--- layouts/special_menu.php
+++ layouts/special_menu.php
@@ -1,29 +1,29 @@
 <ul>
-  <li>
+  <li class="p-3 text-center">
     <a href="home.php">
-      <i class="glyphicon glyphicon-home"></i>
+      <i class="fas fa-home h5 m-0 mr-1"></i>
       <span>Panel de control</span>
     </a>
   </li>
-  <li>
-    <a href="categorie.php" >
-      <i class="glyphicon glyphicon-indent-left"></i>
+  <li class="p-3 text-center">
+    <a href="categorie.php">
+      <i class="fas fa-book h5 m-0 mr-1"></i>
       <span>Categorías</span>
     </a>
   </li>
-  <li>
+  <li class="p-3 text-center">
     <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-th-large"></i>
+      <i class="fas fa-store h5 m-0 mr-1"></i>
       <span>Productos</span>
     </a>
     <ul class="nav submenu">
-       <li><a href="product.php">Administrar productos</a> </li>
-       <li><a href="add_product.php">Agregar producto</a> </li>
-   </ul>
+      <li><a href="product.php">Administrar productos</a> </li>
+      <li><a href="add_product.php">Agregar producto</a> </li>
+    </ul>
   </li>
-  <li>
-    <a href="media.php" >
-      <i class="glyphicon glyphicon-picture"></i>
+  <li class="p-3 text-center">
+    <a href="media.php">
+      <i class="fas fa-image h5 m-0 mr-1"></i>
       <span>Media</span>
     </a>
   </li>
